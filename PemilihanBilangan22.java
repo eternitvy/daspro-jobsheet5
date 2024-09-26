b1be21ce033122b5c5457d08d22c3ca494d2b5ae
eternitvy <crrissant@gmail.com>
2024-09-26 07:59:50 +0700

    hasil modifikasi

--- PemilihanBilangan22.java
+++ PemilihanBilangan22.java
@@ -6,14 +6,9 @@ public class PemilihanBilangan22 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
-
+       String hasil;
+       hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+       System.out.println(angka + " adalah "+ hasil);
+    
     }
 }
